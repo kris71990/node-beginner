7fc97ca12c85b9e5038c2a4e6c985b2874a3e458
Kris Sakarias <ksakarias@gmail.com>
2018-04-14 10:21:35 -0700

    turned server into module

---
+++ index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// Example 3 - server turned into module in server file and then started from index file
+const server = require('./server');
+
+server.start();
--- server.js
+++ server.js
@@ -2,8 +2,37 @@
 
 const http = require('http');
 
-http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write('Hello World');
-  response.end();
-}).listen(8888);
+// Example 1 - server setup
+// http.createServer((request, response) => {
+//   response.writeHead(200, {'Content-Type': 'text/plain'});
+//   response.write('Hello World');
+//   response.end();
+// }).listen(8888);
+
+
+// Example 2 - a different way to structure, with callback to createServer written extracted into its own function
+// function onRequest(request, response) {
+//   console.log('Request received');
+//   response.writeHead(200, {'Content-Type': 'text/plain'});
+//   response.write('Hello World');
+//   response.end();
+// }
+
+// http.createServer(onRequest).listen(8888);
+// console.log('Server has started');
+
+
+// Example 3 - turning our server script in to a module
+function start() {
+  function onRequest(request, response) {
+    console.log('Request received');
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('Hello World');
+    response.end();
+  }
+  http.createServer(onRequest).listen(8888);
+  console.log('Server has started');
+}
+
+exports.start = start;
+
